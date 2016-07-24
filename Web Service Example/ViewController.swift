b7372cc9093cffeb5c274b9009b510f8300cf8a1
jharveymorgan <jharveymorgan@aol.com>
2016-07-23 20:39:05 -0400

    Add AFNetworking to use web service. Use openweather api to get weather info as json. Display description of weather to screen.

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //forecast label
+    @IBOutlet weak var forecastLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // use AFNetworking to use weather service api
+        let manager = AFHTTPSessionManager()
+        // get data from OpenWeatherMap using the GET method
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=73aee2a1963ab8d441b6fd5171d69850",
+                    parameters: nil,
+                    progress: nil,
+                    success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + responseObject.description)
+                            
+                            // get data for forecast from json
+                            if let listOfDays = responseObject["list"] as? [AnyObject] {
+                                if let tomorrow = listOfDays[0] as? [String:AnyObject] {
+                                    if let tomorrowsWeather = tomorrow["weather"] as? [AnyObject] {
+                                        if let firstWeatherOfDay = tomorrowsWeather[0] as? [String:AnyObject] {
+                                            if let forecast = firstWeatherOfDay["description"] as? String? {
+                                                self.forecastLabel.text = forecast
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+            },
+                    failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                        print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Jordan Harvey-Morgan on 7/14/16.
+//  Copyright © 2016 Jordan Harvey-Morgan. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif /* bridge_h */
